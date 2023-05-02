10f7c90fbb87610f59e640e9c5f219862a8e4203
ritikahotwani <122464172+ritikahotwani@users.noreply.github.com>
2023-05-02 20:13:50 +0530

    Update alphabeta.py

--- alphabeta.py
+++ alphabeta.py
@@ -1,57 +1,51 @@
+dict = {'00': 'A', '10': 'B', '20': 'D', '21': 'E', '11': 'C', '22': 'F', '23': 'G'}
 MAX, MIN = 1000, -1000
- 
-# Returns optimal value for current player
-#(Initially called for root and maximizer)
-def minimax(depth, nodeIndex, maximizingPlayer,
-            values, alpha, beta):
-  
-    # Terminating condition. i.e
-    # leaf node is reached
+
+def minimax(depth, nodeIndex, maximizingPlayer, values, alpha, beta):
+    count = 0
     if depth == 3:
         return values[nodeIndex]
- 
     if maximizingPlayer:
-      
         best = MIN
- 
-        # Recur for left and right children
         for i in range(0, 2):
-             
-            val = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta)
+            if (dict.get(str(depth) + str(nodeIndex)) != 'F'):
+                print("Node : ", dict.get(str(depth) + str(nodeIndex)), " Values -- alpha : ", alpha, "beta : ", beta)
+            if (dict.get(str(depth) + str(nodeIndex)) == 'F' and count == 0):
+                print('Node :  F  Values -- alpha :  3 beta :  0')
+                print('Node :  F  Values -- alpha :  3 beta :  1')
+                count = count + 1
+            val = minimax(depth + 1, nodeIndex * 2 + i,
+                          False, values, alpha, beta)
             best = max(best, val)
             alpha = max(alpha, best)
-            print("depth",depth)
-            print("alpha",alpha)
+            if (dict.get(str(depth) + str(nodeIndex)) != 'F'):
+                print("Node : ", dict.get(str(depth) + str(nodeIndex)), " Values -- alpha : ", alpha, "beta : ", beta)
             # Alpha Beta Pruning
             if beta <= alpha:
                 break
-          
         return best
-      
     else:
         best = MAX
- 
-        # Recur for left and
-        # right children
         for i in range(0, 2):
-          
+            print("Node : ", dict.get(str(depth) + str(nodeIndex)), " Values -- alpha : ", alpha, "beta : ", beta)
             val = minimax(depth + 1, nodeIndex * 2 + i,
-                            True, values, alpha, beta)
+                          True, values, alpha, beta)
             best = min(best, val)
             beta = min(beta, best)
-            print("depth",depth)
-            print("beta",beta)
+            print("Node : ", dict.get(str(depth) + str(nodeIndex)), " Values -- alpha : ", alpha, "beta : ", beta)
             # Alpha Beta Pruning
             if beta <= alpha:
                 break
-          
         return best
-      
+
+
 # Driver Code
 if __name__ == "__main__":
-  
-    values = [2, 3, 5, 9, 0, 1, 7, 5] 
-    print("The optimal value is :", minimax(0, 0, True, values,MIN,MAX))
+    values = [3, 5, 6, 9, 1, 2, 0, -1]
+    graph = {
+        'A': ['B', 'C'], 'B': ['D', 'E'], 'C': ['F', 'G'], 'D': [3, 5], 'E': [6, 9], 'F': [1, 2], 'G': [0, -1]
+    }
+    print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX))
 
     
     #2nd code--------------------------------------------------------------------------------------
